3be411c805c8ca7b815dc458faafa8f8f75b8778
ismael hernan uriarte <ismaelu064@gmail.com>
2024-02-04 09:57:18 -0300

    realice cards opcionales

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
-import {View, Text, StyleSheet } from 'react-native';
+import {View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Hola Coder, Bienvenidos a mi App!!</Text>
+    <View style={styles.containerPadre}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder='Ingresar tarea' />
+        <Button title='Add' />
+      </View>
+
+      <View style={styles.cardsContainer}>
+        <View style={styles.tarjetaProd}>
+          <Text style={styles.texto}>Tarea 1</Text>
+          <Button title='Delete'/>
+        </View>
+        
+        <View style={styles.tarjetaProd}>
+          <Text style={styles.texto}>Tarea 2</Text>
+          <Button title='Delete'/>
+        </View>
+
+        <View style={styles.tarjetaProd}>
+          <Text style={styles.texto}>Tarea 3</Text>
+          <Button title='Delete'/>
+        </View>
+      </View>
+      
     </View>
   );
 }
@@ -11,11 +32,45 @@ const App = () => {
 export default App
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor:"#11212D",
-    flex:1,
+  containerPadre:{
+    backgroundColor:"#DBDBDB", 
+    flex:1, 
+    paddingTop:30
   },
+
+  inputContainer:{
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-around",
+    
+
+  },
+  input:{
+    width:250,
+    borderWidth:2,
+    margin:10,
+    paddingVertical:5,
+    paddingHorizontal:10
+  },
+
   texto:{
-    color:"white"
-  }
+    color:"white",
+    fontSize:16,
+    width:"70%"
+  },
+
+  cardsContainer:{
+    alignItems:"center",
+    gap:25,
+    padding:10,
+    marginTop:20
+  },
+
+  tarjetaProd:{
+    flexDirection:"row",
+    backgroundColor:"#204D79",
+    padding:10,
+    alignItems:"center",
+    borderRadius:10
+  },
 })
